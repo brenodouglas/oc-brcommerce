47309a44ab2816537f7c4feb39caa9b63bad437d
Breno <breno.douglas@paieterno.com.br>
2016-03-18 20:09:27 +0000

    Add migrations for customer and customer group

---
+++ brcommerce/updates/create_customer_group_migration.php
@@ -0,0 +1,26 @@
+<?php namespace BrenoDouglasAraujoSouza\BrCommerce\Updates;
+
+use Illuminate\Database\Schema\Blueprint;
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class CreateCustomerGroupMigration extends Migration
+{
+    public function up()
+    {
+        Schema::create('brenodouglasaraujosouza_brcommerce_customer_group', function(Blueprint $table)
+        {
+            $table->increments("id");
+            $table->string("name");
+            $table->text('description');
+            $table->boolean("direct_approve");
+
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('brenodouglasaraujosouza_brcommerce_customer_group');
+    }
+}
---
+++ brcommerce/updates/create_customer_migration.php
@@ -0,0 +1,37 @@
+<?php namespace BrenoDouglasAraujoSouza\BrCommerce\Updates;
+
+use Illuminate\Database\Schema\Blueprint;
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class CreateCustomerMigration extends Migration
+{
+    public function up()
+    {
+        Schema::create('brenodouglasaraujosouza_brcommerce_customer', function(Blueprint $table)
+        {
+            $table->increments("id");
+            $table->string("first_name");
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('telephone');
+            $table->string('fax');
+            $table->string('password');
+            $table->string('salt');
+            $table->boolean('newsletter');
+            $table->integer('status');
+            $table->integer('email_active');
+
+            $table->integer('group_id')->unsigned();
+            $table->foreign('group_id')->references('id')->on('brenodouglasaraujosouza_brcommerce_customer_group');
+            $table->index('group_id', 'custome_customer_group_index');
+
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('brenodouglasaraujosouza_brcommerce_customer');
+    }
+}

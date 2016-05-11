176b2f62020c12ee81252500be31ac5d91c7310b
Breno Douglas <breno.douglas@paieterno.com.br>
2016-05-11 18:40:15 +0000

    Add order and invoices migrations

---
+++ brcommerce/updates/create_orders_table.php
@@ -0,0 +1,22 @@
+<?php namespace Brenodouglasaraujosouza\Brcommerce\Updates;
+
+use Schema;
+use October\Rain\Database\Schema\Blueprint;
+use October\Rain\Database\Updates\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('brenodouglasaraujosouza_brcommerce_orders', function(Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('brenodouglasaraujosouza_brcommerce_orders');
+    }
+}

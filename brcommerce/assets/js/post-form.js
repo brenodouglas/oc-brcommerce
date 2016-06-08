1e456447d192b6de0682a86d1f837387528f7c16
Breno Douglas Araujo Souza <breno_douglas@hotmail.com>
2016-06-07 21:30:53 -0300

    Form for digital

--- brcommerce/assets/js/post-form.js
+++ brcommerce/assets/js/post-form.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
     $('#Form-secondaryTabs .tab-pane.layout-cell').addClass('padded-pane');
+    $('#Form-field-Product-sku').keypress(function(e){
+        if(e.which === 13){
+            return false;
+        }
+    });
 });

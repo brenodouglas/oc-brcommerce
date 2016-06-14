e96d21f1cc6b4e5aa053d0cba10f1093d165551b
BrenoDouglas <bdouglasans@gmail.com>
2016-06-14 02:13:59 +0000

    Correções e adição de filtro em utils

--- brcommerce/utils/Filter.php
+++ brcommerce/utils/Filter.php
@@ -6,20 +6,39 @@ use BrenoDouglasAraujoSouza\BrCommerce\Models\Product;
 class Filter
 {
     const KEY = 'filers-br-commerce-0-1';
+    const KEY_ORDER = 'filters-br-commerce-order-0-10';
+
+    private $order;
     private $collection;
 
     public function __construct()
     {
         $this->collection = new \ArrayIterator();
+        $this->order = Session::get(self::KEY_ORDER);
+        
+        if(! $this->order) 
+            $this->addOrder('created_at');
+
         $this->collection->unserialize(Session::get(self::KEY));
     }
 
-    private function isValueIdentical($item, $value) {
+    private function isValueIdentical($item, $value)
+    {
         return (! is_array($item['value']) &&  $item['value'] == $value)
                 || (is_array($item) && $item['value'][0] == $value[0] && $item['value'][1] == $item[1]);
     }
 
-    public function add($name, $value)
+    public function inFilter (string $name, string $value) : bool
+    {
+        foreach($this->getFilters() as $filterItem) {
+            if($filterItem['name'] == $name && $filterItem['value'] == $value)
+                return true;
+        }
+
+        return false;
+    }
+
+    public function add(string $name, string $value)
     {
         foreach($this->collection as $item) {
             if($item['name'] == $name && $this->isValueIdentical($item, $value))
@@ -29,6 +48,43 @@ class Filter
         $this->collection->append(['name' => $name, 'value' => $value]);
 
         Session::put(self::KEY, $this->collection->serialize());
+    }
+
+    public function addOrder($value) 
+    {
+        $this->order = $value;
+        Session::put(self::KEY_ORDER, $value);
+    }
+
+    public function getOrder()
+    {
+        return $this->order;
+    }
+
+    public function getOrderType()
+    {
+        $order = $this->getOrder();
+        switch ($order) {
+            case 'min_price':
+                return 'ASC';
+            case 'max_price':
+                return 'DESC';
+            default:
+                return 'DESC';
+        }
+    }
+
+    public function getOrderColumn()
+    {
+        $order = $this->getOrder();
+        switch ($order) {
+            case 'min_price':
+                return 'brenodouglasaraujosouza_brcommerce_product.price';
+            case 'max_price':
+                return 'brenodouglasaraujosouza_brcommerce_product.price';
+            default:
+                return 'brenodouglasaraujosouza_brcommerce_product.created_at';
+        }    
     }
 
     public function getFilters()
